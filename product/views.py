bb963b90d53655b967789544194ad2182e48b178
olsi10 <wangbampang2035@gmail.com>
2023-04-13 13:44:12 +0900

    views ProductDetailView

--- product/views.py
+++ product/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from product.models import Product
 
@@ -10,3 +10,7 @@ class ProductListView(ListView):
     model = Product
     # 보여줄 거는 프로덕트다.
     # 'product_list', {'product_list' : Product.object.all() }
+
+class ProductDetailView(DetailView):
+    model = Product
+    # 'product_detail.html', { 'product' : Product.object.get(pk=pk) } pk -> primary key
